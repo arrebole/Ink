1f873718b316c392ee33255cca0ef3ae4a3ad092
concurrent <concurrent.exec@gmail.com>
2019-11-24 18:32:23 +0800

    leetcode.9

--- src/Algorithm/Challenge/leetcode.1/two_sum.c
+++ src/Algorithm/Challenge/leetcode.1/two_sum.c
@@ -1,10 +1,11 @@
 // 求两数之和
 #include <stdlib.h>
 
-int* createResult(int a, int b){
+int* createResult(int a, int b, int* returnSize){
     int* result = (int*)malloc(sizeof(int)*2);
     result[0] = a;
     result[1] = b;
+    *returnSize = 2;
     return result;
 }
 
@@ -15,9 +16,8 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 
     for(int i = hi; i >= 0; i--){
         for(int j = 0; j < i; j++){
-            if (nums[i] + nums[j] == target){
-                *returnSize = 2;
-                return createResult(j, i);
+            if (nums[i] + nums[j] == target) {
+                return createResult(j, i, returnSize);
             }
         }
     }
---
+++ src/Algorithm/Challenge/leetcode.9/is_palindrome_test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "./is_palindrome.c"
+
+int main(){
+    printf("%d", isPalindrome(10));
+    return 0;
+}
