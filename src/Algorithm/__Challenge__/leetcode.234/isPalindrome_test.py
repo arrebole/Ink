4c080767bd3a31d4796d5f22b107de7bd1f6a788
concurrent <concurrent.exec@gmail.com>
2020-01-08 15:03:38 +0800

    Add: solve leetcode.234

---
+++ src/Algorithm/__Challenge__/leetcode.234/isPalindrome_test.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+
+import unittest
+from isPalindrome import Solution
+
+class TestSolution(unittest.TestCase):
+    def test_isPalindrome(self):
+        self.assertEqual(Solution().isPalindrome(), False)
+
+
+if __name__ == "__main__"
+    unittest.main()
