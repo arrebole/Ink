3128b635c5045c00fe768314535bbcfa94b10e22
concurrent <concurrent.exec@gmail.com>
2020-01-09 18:42:18 +0800

    Add: 算法-近似算法-平分法求非线性方程的根

--- src/Algorithm/10.Beyond-The-Limits/Approximate/Solving-Equations/bisection.js
+++ src/Algorithm/10.Beyond-The-Limits/Approximate/Solving-Equations/bisection.js
@@ -3,8 +3,27 @@
 // bisection 解非线性方程的平分算法
 // fn: 求fn的一个根
 // a,b: f(a)*f(b) < 0
-// epx: 绝对误差的上界
+// eps: 绝对误差的上界
 // N： 迭代次数的上限
-function bisection(fn, a, b, epx, N){
+function bisection(fn, a, b, eps, N) {
+    // 统计迭代次数
+    let n = 0;
+    while (n++ <= N) {
+        // 寻找中点
+        let x0 = average(a, b)
 
+        // 如果误差精度足够 或者刚好为根 则返回结果 
+        if ((x0 - a) < eps || fn(x0) == 0) return x0;
+
+        // 往根处靠近
+        if (fn(x0) * fn(a) < 0) b = x0;
+        else                    a = x0;
+    }
+    return average(a, b);
 }
+
+function average(a, b) {
+    return (a + b) / 2;
+}
+
+module.exports = bisection
