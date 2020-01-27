b7ad3374db5d770d0e2ce1cbf5ea98cd940975ee
concurrent <concurrent.exec@gmail.com>
2020-01-27 14:24:15 +0800

    Add: solve leetcode.39

---
+++ src/Algorithm/__Challenge__/leetcode.39/combinationSum.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"sort"
+)
+
+func combinationSum(candidates []int, target int) [][]int {
+	var result [][]int
+	sort.Ints(candidates)
+	backtrack(&result, candidates, []int{}, 0, target)
+	return result
+}
+
+func backtrack(out *[][]int, candidates, curr []int, i, target int) {
+	if target == 0 {
+		*out = append(*out, append([]int{}, curr...))
+		return
+	}
+
+	for j := i; j < len(candidates); j++ {
+		if (target - candidates[i]) >= 0 {
+			backtrack(out, candidates, append(curr, candidates[j]), j, target-candidates[j])
+		}
+	}
+}
---
+++ src/Algorithm/__Challenge__/leetcode.39/combinationSum_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestCombinationSum(t *testing.T) {
+	combinationSum([]int{7, 3, 2}, 18)
+}
