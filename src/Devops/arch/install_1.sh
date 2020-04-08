c85af3811758aed35b89b14b05eebdfdfa4ee88c
arrebole <c.y.concurrent@gmail.com>
2020-04-08 14:44:58 +0800

    Update install_1.sh

--- src/Devops/arch/install_1.sh
+++ src/Devops/arch/install_1.sh
@@ -18,7 +18,7 @@ mkdir -p /mnt/boot/EFI
 mount /dev/sda1 /mnt/boot/EFI
 echo "config mirr"
 sed -i '/China/!{n;/Server/s/^/#/};t;n' /etc/pacman.d/mirrorlist
-echo "pacstrap /mnt base"
-pacstrap /mnt base
+echo "pacstrap /mnt base linux"
+pacstrap /mnt base linux
 echo "genfstab -U /mnt >> /mnt/etc/fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
