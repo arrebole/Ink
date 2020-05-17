b7db5feb75397c63db8b129e7d634a5129ef88a0
chaoyi.li <chaoyi.li@upai.com>
2020-05-18 02:33:32 +0800

    feat: add archlinux sway warbar

--- src/Devops/kernel/archlinux/install_sway.sh
+++ src/Devops/kernel/archlinux/install_sway.sh
@@ -2,4 +2,4 @@ echo "install drivers"
 pacman -S xf86-video-intel # xf86-video-nouveau
 
 echo "install desktop"
-pacman -S sway alacritty wofi xorg-server-xwayland
+pacman -S sway waybar alacritty wofi xorg-server-xwayland
