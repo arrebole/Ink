715dc437d6711afdf7da749640395e73faf51cea
chaoyi.li <chaoyi.li@upai.com>
2020-05-17 08:05:47 +0800

    feat: archlinux config

--- src/Devops/kernel/archlinux/install_sway.sh
+++ src/Devops/kernel/archlinux/install_sway.sh
@@ -3,20 +3,3 @@ pacman -S xf86-video-intel # xf86-video-nouveau
 
 echo "install desktop"
 pacman -S sway alacritty wofi xorg-server-xwayland
-
-#-->
-#   path ~/.config/sway/config
-#   
-#   设置间隔
-#   gaps inner 10
-#   gaps outer 5
-#
-#   设置无边框
-#   default_border pixel
-#  
-#   终端与启动器
-#   set $menu dmenu_path | wofi --show drun -i | xargs swaymsg exec --
-#   set $term alacritty
-#   
-#
-#<--
