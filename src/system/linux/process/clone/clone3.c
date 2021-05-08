a72d63866ec37e32b3cfc84f4e9464262ac102f5
arrebole <concurrent.exec@gmail.com>
2021-05-08 14:30:22 +0800

    system/linux: not fix clone3

--- src/system/linux/process/clone/clone3.c
+++ src/system/linux/process/clone/clone3.c
@@ -1,8 +1,10 @@
 #define _GNU_SOURCE
-#include <syscall.h>
 #include <malloc.h>
-#include <unistd.h>
 #include <sched.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <syscall.h>
+#include <unistd.h>
 
 typedef unsigned long long u64;
 
@@ -17,41 +19,41 @@ struct clone_args {
   u64 tls;          /* Location of new TLS */
   u64 set_tid;      /* Pointer to a pid_t array (since Linux 5.5) */
   u64 set_tid_size; /* Number of elements in set_tid (since Linux 5.5) */
-  u64 cgroup;       /* File descriptor for target cgroup of child (since Linux 5.7) */
+  u64 cgroup; /* File descriptor for target cgroup of child (since Linux 5.7) */
 };
 
-long clone3(struct clone_args *cl_args, unsigned long size) {
-    return syscall(SYS_clone3, cl_args, size);
+#define STACK_SIZE (1024 * 1024)
+
+long clone3(struct clone_args* cl_args, unsigned long size) {
+  return syscall(SYS_clone3, cl_args, size);
 }
 
 int main(int argc, const char* argv[]) {
-    int stack_size = 1024;
-    char* stack = malloc(stack_size);
-
-    struct clone_args cl_args = {
-        .flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND,
-        .pidfd = (u64)NULL,
-        .child_tid = (u64)NULL,
-        .parent_tid = (u64)NULL,
-        .exit_signal = (u64)0,
-        .stack = (u64)stack,
-        .stack_size = (u64)stack_size,
-        .tls = (u64)NULL,
-        .set_tid = (u64)NULL,
-        .set_tid_size = 0,
-        .cgroup = 0,
-    };
-
-    int pid = clone3(&cl_args, sizeof(cl_args));
-    if (pid < 0) perror("clone3");
-
-    if (pid == 0) {
-        printf("\nclone\n");
-    }
-    if (pid > 0) {
-        printf("\nmain\n");
-        sleep(2);
-    }
-
+  char* stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
+  if (stack == MAP_FAILED) {
+    perror("mmap");
     return 0;
+  }
+
+  struct clone_args cl_args = {
+      .flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND |
+               CLONE_SYSVSEM | CLONE_THREAD,
+      .stack = (u64)stack + STACK_SIZE,
+      .stack_size = STACK_SIZE,
+      .exit_signal = SIGCHLD,
+  };
+
+  int pid = clone3(&cl_args, sizeof(cl_args));
+  if (pid < 0) perror("clone3");
+
+  if (pid == 0) {
+    printf("I am the child, my PID is %d\n", getpid());
+  }
+  if (pid > 0) {
+    printf("I am the parent (%d). My child's pid is %d\n", getpid(), pid);
+    sleep(2);
+  }
+
+  return 0;
 }
